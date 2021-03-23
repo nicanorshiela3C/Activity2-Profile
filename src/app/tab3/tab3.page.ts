803b1ab69376f617c83816623350dafb221e429f
unknown <shiela.nicanor@evsu.edu.ph>
2021-03-23 09:28:02 +0800

    Act3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactServiceService } from '../services/contact-service.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contName = "";
+  contLastname = "";
+  contNumber = ""
+  contacts: any = [];
 
-  constructor() {}
+  constructor(public contactService: ContactServiceService, public alertController: AlertController) { }
+
+  saveC() {
+    let contact = {
+      name: this.contName,
+      lastname: this.contLastname,
+      number: this.contNumber
+      
+    }
+    this.contacts.push(contact);
+    this.clearField();
+    console.log(this.contacts);
+  }
+
+  clearField() {
+    this.contName = "";
+    this.contLastname = "";
+    this.contNumber = "";
+  }
+  async delete(cont){
+    const alert = await this.alertController.create({
+      header: 'Delete?',
+      message: '',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'icon-color',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text:'Yes',
+          cssClass:'icon-color',
+          handler: () => {
+            let index = this.contacts.indexOf(cont);
+            if(index > -1){
+              this.contacts.splice(index,1);
+              console.log('Yes');
+            }
+          }
+        }
+      ]
+    });
+      await alert.present();
+  }
 
 }
